36da1a0ab4e75bae7659bdce1819701b274be56f
jonuchauhan <jonuchauhan@gmail.com>
2023-01-07 12:06:45 +0200

    graceful shutdown of consumer

---
+++ kafka_course/src/main/java/io/conduktor/com/ConsumerWithGracefulShutdown.java
@@ -0,0 +1,69 @@
+package io.conduktor.com;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerWithGracefulShutdown {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerWithGracefulShutdown.class.getName());
+
+    public static void main(String[] args) {
+        String topic="first_topic";
+        String group = "third_group";
+        log.info("setting up properties");
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,group);
+
+
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String,String>(properties);
+
+        final Thread mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            public void run(){
+                log.info("shutdown detected");
+                consumer.wakeup();
+                try{
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        consumer.subscribe(Arrays.asList(topic));
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> consumerrecords = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : consumerrecords) {
+                    log.info(record.key());
+                }
+
+            }
+        }
+        catch (WakeupException e){
+            log.info("wakeup exception detected");
+        }
+        catch (Exception e){
+            log.info("Unexpected exception occured");
+        }finally {
+            log.info("closing consumer");
+            consumer.close();
+        }
+
+
+
+    }
+}
